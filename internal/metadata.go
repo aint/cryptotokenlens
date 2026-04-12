46bfd63a6880a96311e6190c4bdd4523ac53a512
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2026-04-12 23:15:04 +0300

    Calc bought amount

--- internal/metadata.go
+++ internal/metadata.go
@@ -10,6 +10,25 @@ import (
 
 	"github.com/aint/cryptotokenlens/internal/polygonscan"
 )
+
+func BoughtAmount(txs []polygonscan.TokenTransfer, tokenAddr string) *big.Int {
+	boughtAmount := big.NewInt(0)
+	for _, t := range txs {
+		v, ok := new(big.Int).SetString(t.Value, 10)
+		if !ok {
+			fmt.Fprintf(os.Stderr, "parse value %q\n", t.Value)
+			continue
+		}
+
+		from := strings.ToLower(t.From)
+		if from == tokenAddr {
+			boughtAmount.Add(boughtAmount, v)
+		}
+	}
+
+	return boughtAmount
+}
+
 func GetDecimal(txs []polygonscan.TokenTransfer) (uint8, error) {
 	if len(txs) == 0 {
 		return 0, errors.New("no transactions found")
